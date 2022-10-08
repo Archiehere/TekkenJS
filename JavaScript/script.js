8755b31e960b536471c717c6597b262d51c2f5cc
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2022-10-08 19:05:29 +0530

    Add background using sprite

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,28 @@
 // Ratnesh commit
 //ARCHAS COMMIT 
 //commit 2
+
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let backgroundImage = new Image();
+backgroundImage.src = "images/background/backgroundSprite.png"
+
+let backgroundImageOver = new Image();
+backgroundImageOver.src = "images/background/backgroundSprite-removebg.png"
+
+function background() {
+    ctx.drawImage(backgroundImage, 10, 8, 419, 224, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(backgroundImage, 205, 255, 672, 33, 0, 570, canvas.width, canvas.height-570);
+    ctx.drawImage(backgroundImageOver, 18, 340, 672, 200, 0, 0, canvas.width, 580);
+}
+
+function animate() {
+    background();
+    requestAnimationFrame(animate);
+}
+
+animate();
+
