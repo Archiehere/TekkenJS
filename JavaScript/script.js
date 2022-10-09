355dd0e883f1a6393c4b20a284f8fc4ce28ed0e3
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2022-10-09 11:58:26 +0530

    Add health bars

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,78 +6,95 @@ const BGcanvas = document.getElementById('myCanvas');
 const BGctx = BGcanvas.getContext('2d');
 BGcanvas.width = window.innerWidth;
 BGcanvas.height = window.innerHeight;
-let speed=6,leftlocation=150;
-let toplocation=380;
+let speed = 10, leftlocation = 150;
+let toplocation = 380;
+
+// Temporary variable for player one health
+let playerOne = {
+    health: 100,
+    healthBar: document.getElementById('health-one')
+}
+
+let playerTwo = {
+    health: 100,
+    healthBar: document.getElementById('health-two')
+}
+
+
 // Event Listeners
 window.addEventListener('keydown', function (event) {
     switch (event.key.toLocaleLowerCase()) {
         case 'w':
             // Event
-            playerState="jump"
-            while(toplocation>300)
-            {toplocation-=speed;
-            canvas.style.top= toplocation + "px";}
-            playerTwo.health--;
+            playerState = "jump"
+            while (toplocation > 300) {
+                toplocation -= speed;
+                canvas.style.top = toplocation + "px";
+            }
 
             break;
         case 'a':
             // Event
-            playerState="backward";
-            if(leftlocation>10)
-            leftlocation-=speed;
-            canvas.style.left= leftlocation + "px" ;
-            playerOne.health--;
-            
+            playerState = "backward";
+            if (leftlocation > 10)
+                leftlocation -= speed;
+            canvas.style.left = leftlocation + "px";
+
+
             break;
         case 's':
             // Event
-            playerState="crouch";
+            playerState = "crouch";
             break;
         case 'd':
             // Event
-            playerState="forward";
-            if(leftlocation<1200)
-            leftlocation+=speed;
-            canvas.style.left= leftlocation + "px" ;
+            playerState = "forward";
+            if (leftlocation < 1200)
+                leftlocation += speed;
+            canvas.style.left = leftlocation + "px";
             break;
         case 'z':
             // Event
-            playerState="kick";
+            playerState = "kick";
             break;
         case 'x':
             // Event
-            playerState="punch"
+            playerState = "punch"
             break;
         case 'arrowleft':
             // Event
-            playerState="forward";
+            playerState = "forward";
             break;
         case 'arrowright':
             // Event
-            playerState="backward";
+            playerState = "backward";
             break;
         case 'arrowup':
             // Event
-            playerState="jump";
+            playerState = "jump";
             break;
         case 'arrowdown':
             // Event
-            playerState="crouch";
+            playerState = "crouch";
             break;
         case 'n':
             // Event
+            playerOne.health -= 10;
+            playerOne.healthBar.style.width = playerOne.health + '%';
             break;
         case 'm':
             // Event
+            playerTwo.health -= 10;
+            playerTwo.healthBar.style.width = playerTwo.health + '%';
+            playerTwo.healthBar.style.marginLeft = 100 - playerTwo.health + '%';
             break;
     }
 })
-window.addEventListener('keyup', function (){
-playerState="idle";
-    while(toplocation!=380)
-    {
-        toplocation+=speed;
-        canvas.style.top=toplocation + "px";
+window.addEventListener('keyup', function () {
+    playerState = "idle";
+    while (toplocation != 380) {
+        toplocation += speed;
+        canvas.style.top = toplocation + "px";
     }
 })
 
@@ -93,32 +110,8 @@ function background() {
     BGctx.drawImage(backgroundImageOver, 18, 340, 672, 200, 0, 0, BGcanvas.width, 580);
 }
 
- // Temporary variable for player one health
-let playerOne = {
-    health: 100
-}
 
-let playerTwo = {
-    health: 100
-}
 
-function healthBarOne() {
-    BGctx.strokeStyle = "black";
-    BGctx.lineWidth = 5;
-    BGctx.fillStyle = `hsl(${Math.max(0, playerOne.health)}, 100%, 50%)`;
-    BGctx.rect(BGcanvas.width/10, BGcanvas.height/10, BGcanvas.width/3, 20);
-    BGctx.stroke();
-    BGctx.fill();
-}
-
-function healthBarTwo() {
-    BGctx.strokeStyle = "black";
-    BGctx.lineWidth = 5;
-    BGctx.fillStyle = `hsl(${Math.max(0, playerTwo.health)}, 100%, 50%)`;
-    BGctx.rect(BGcanvas.width/1.8 , BGcanvas.height/10, BGcanvas.width/3, 20);
-    BGctx.stroke();
-    BGctx.fill();
-}
 
 let playerState = 'intro';
 // const dropdown = document.getElementById('animations');
@@ -132,123 +125,121 @@ const CANVAS_HEIGHT = canvas.height = 350;
 const playerImage = new Image();
 playerImage.src = 'images/Ryu.gif';
 let spriteWidth = 62;
-let spriteHeight =  115;
+let spriteHeight = 115;
 let gameFrame = 0;
-const staggerFrames = 20;
+const staggerFrames = 8;
 const spriteAnimations = [];
-const animationStates =[
+const animationStates = [
     {
-    name: 'intro',
-    frames: 12,
-    framexposition: [18 , 92 , 167 ,246 ,322,322 ,391 ,391,466 ,466 ,536, 536]  ,
-    frameswidth:    [69 , 69 , 69  ,69  ,69 ,69  ,69  ,69 ,69  ,69  , 69, 69 ] ,
-    framesheight:   [110, 110, 110 ,110 ,110,110 ,110, 110,110, 110 ,110, 110] ,
-    frameyposition: 0,
+        name: 'intro',
+        frames: 12,
+        framexposition: [18, 92, 167, 246, 322, 322, 391, 391, 466, 466, 536, 536],
+        frameswidth: [69, 69, 69, 69, 69, 69, 69, 69, 69, 69, 69, 69],
+        framesheight: [110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110],
+        frameyposition: 0,
     },
     {
-    name: 'idle',
-    frames: 6,
-    framexposition: [109 , 189 , 269 ,349 ,425 ,504]  ,
-    frameswidth:    [73  , 73  , 73  ,73  ,73  ,73 ] ,
-    framesheight:   [110 , 110 , 110 ,110 ,110 ,110] ,
-    frameyposition: spriteHeight*1,
+        name: 'idle',
+        frames: 6,
+        framexposition: [109, 189, 269, 349, 425, 504],
+        frameswidth: [73, 73, 73, 73, 73, 73],
+        framesheight: [110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 1,
     },
     {
-    
-    name: 'forward',
-    frames: 6,
-    framexposition: [63  , 144 , 226 ,307 ,376 ,453]  ,
-    frameswidth:    [70  , 70  , 70  ,70  ,70  ,70 ] ,
-    framesheight:   [110 , 110 , 110 ,110 ,110 ,110] ,
-    frameyposition: spriteHeight*2,
+
+        name: 'forward',
+        frames: 6,
+        framexposition: [63, 144, 226, 307, 376, 453],
+        frameswidth: [70, 70, 70, 70, 70, 70],
+        framesheight: [110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 2,
     },
     {
         name: 'backward',
         frames: 6,
-        framexposition: [64  , 140 , 219 ,292 ,368 ,455]  ,
-        frameswidth:    [70  , 70  , 70  ,70  ,70  ,70 ] ,
-        framesheight:   [110 , 110 , 110 ,110 ,110 ,110] ,   
-        frameyposition: spriteHeight*3,
+        framexposition: [64, 140, 219, 292, 368, 455],
+        frameswidth: [70, 70, 70, 70, 70, 70],
+        framesheight: [110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 3,
     },
     {
         name: 'jump',
         frames: 7,
-        framexposition: [36  , 109 , 189 , 269 ,349 ,425 ,504]  ,
-        frameswidth:    [73  , 73  , 73  , 73  ,73  ,73  ,73 ] ,
-        framesheight:   [110 , 110 , 110 , 110 ,110 ,110 ,110] ,
-        frameyposition: spriteHeight*4,
+        framexposition: [36, 109, 189, 269, 349, 425, 504],
+        frameswidth: [73, 73, 73, 73, 73, 73, 73],
+        framesheight: [110, 110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 4,
     },
     {
         name: 'punch',
         frames: 6,
-        framexposition: [25  , 106 , 189 , 309 ,402 ,517 ,504]  ,
-        frameswidth:    [80  , 80  , 108  , 80 ,108  ,80  ,80 ] ,
-        framesheight:   [110 , 110 , 110 , 110 ,110 ,110 ,110] ,
-        frameyposition: spriteHeight*7+5,
+        framexposition: [25, 106, 189, 309, 402, 517, 504],
+        frameswidth: [80, 80, 108, 80, 108, 80, 80],
+        framesheight: [110, 110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 7 + 5,
     },
     {
         name: 'kick',
         frames: 6,
-        framexposition: [5  , 96  , 175 , 269 ,342 ,455 ,535]  ,
-        frameswidth:    [73  , 73  , 90  , 73  ,101  ,73  ,73 ] ,
-        framesheight:   [110 , 110 , 110 , 110 ,110 ,110 ,110] ,
-        frameyposition: spriteHeight*8,
+        framexposition: [5, 96, 175, 269, 342, 455, 535],
+        frameswidth: [73, 73, 90, 73, 101, 73, 73],
+        framesheight: [110, 110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 8,
     },
     {
-    name: 'crouch',
-    frames: 4,
-    framexposition: [8 , 8 , 88 ,88 ,425 ,504]  ,
-    frameswidth:    [73  , 73  , 73  ,73  ,73  ,73 ] ,
-    framesheight:   [110 , 110 , 110 ,110 ,110 ,110] ,
-    frameyposition: spriteHeight*5,
+        name: 'crouch',
+        frames: 4,
+        framexposition: [8, 8, 88, 88, 425, 504],
+        frameswidth: [73, 73, 73, 73, 73, 73],
+        framesheight: [110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 5,
     },
     {
         name: 'gethit',
         frames: 3,
-        framexposition: [222 , 309 , 398 ,88 ,425 ,504]  ,
-        frameswidth:    [73  , 73  , 73  ,73  ,73  ,73 ] ,
-        framesheight:   [110 , 110 , 110 ,110 ,110 ,110] ,
-        frameyposition: spriteHeight*21,
+        framexposition: [222, 309, 398, 88, 425, 504],
+        frameswidth: [73, 73, 73, 73, 73, 73],
+        framesheight: [110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 21,
     },
     {
         name: 'ko',
         frames: 3,
-        framexposition: [25 , 156 , 293 ,88 ,425 ,504]  ,
-        frameswidth:    [130  , 130  , 130  ,73  ,73  ,73 ] ,
-        framesheight:   [110 , 110 , 110 ,110 ,110 ,110] ,
-        frameyposition: spriteHeight*23,
+        framexposition: [25, 156, 293, 88, 425, 504],
+        frameswidth: [130, 130, 130, 73, 73, 73],
+        framesheight: [110, 110, 110, 110, 110, 110],
+        frameyposition: spriteHeight * 23,
     }
-    ];
+];
 
 
 
 animationStates.forEach((state, index) => {
-        let frames = {
+    let frames = {
         loc: [],
-        }
-        for (let j = 0; j < state.frames; j ++){
+    }
+    for (let j = 0; j < state.frames; j++) {
         let positionx = state.framexposition[j];
         let positiony = state.frameyposition;
-        frames.loc.push({x: positionx, y: positiony ,framewidth:state.frameswidth[j],frameheight:state.framesheight[j]});
-        }
-        spriteAnimations[state.name] = frames;
-        });
-        // console.log(spriteAnimations);
-        // console.log(animationStates);
-        function animate(){
-            
-        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        background();
-        healthBarOne();
-        healthBarTwo();
-        let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
-        // let framex = spacebeginning +(spriteWidth+spacebetween) * position;
-        let framey = spriteAnimations[playerState].loc[position].y ;
-        let framex = spriteAnimations[playerState].loc[position].x ;
-        spriteWidth = spriteAnimations[playerState].loc[position].framewidth;
-        spriteHeight = spriteAnimations[playerState].loc[position].frameheight;
-        ctx.drawImage(playerImage, framex, framey, spriteWidth, spriteHeight, 50, 50,CANVAS_WIDTH-50,CANVAS_HEIGHT-50);
-        gameFrame ++;
-        requestAnimationFrame(animate);
-        };
-        animate();
+        frames.loc.push({ x: positionx, y: positiony, framewidth: state.frameswidth[j], frameheight: state.framesheight[j] });
+    }
+    spriteAnimations[state.name] = frames;
+});
+// console.log(spriteAnimations);
+// console.log(animationStates);
+function animate() {
+
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    background();
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
+    // let framex = spacebeginning +(spriteWidth+spacebetween) * position;
+    let framey = spriteAnimations[playerState].loc[position].y;
+    let framex = spriteAnimations[playerState].loc[position].x;
+    spriteWidth = spriteAnimations[playerState].loc[position].framewidth;
+    spriteHeight = spriteAnimations[playerState].loc[position].frameheight;
+    ctx.drawImage(playerImage, framex, framey, spriteWidth, spriteHeight, 50, 50, CANVAS_WIDTH - 50, CANVAS_HEIGHT - 50);
+    gameFrame++;
+    requestAnimationFrame(animate);
+};
+animate();
